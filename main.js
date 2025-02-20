5a659e9c9f32008d9493035a37a44a4e0478df1c
afei <917566188@qq.com>
2025-02-20 16:58:29 +0800

    uview插件导入

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
+import uView from './uni_modules/vk-uview-ui';
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
@@ -15,6 +16,7 @@ app.$mount()
 import { createSSRApp } from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
+  app.use(uView)
   return {
     app
   }
